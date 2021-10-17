34def03871b8b15a927cd7aca6b7fe48a0cae1cd
Changseo Jang <changseo_jang@korea.ac.kr>
2021-10-17 19:11:57 +0900

    add logger

--- imgui-rs-d3d12-examples/src/main.rs
+++ imgui-rs-d3d12-examples/src/main.rs
@@ -7,15 +7,19 @@ use winit::{
     },
     dpi::PhysicalSize,
 };
+use simple_logger::SimpleLogger;
+use log;
 
 const WIDTH: u32 = 1024;
 const HEIGHT: u32 = 768;
 
 fn main() {
-    create_window("Test Window");
+    SimpleLogger::new().init().unwrap();
+    let (_window, _event_loop) = create_window("Test Window").unwrap();
 }
 
 fn create_window(title: &str) -> Result<(Window, EventLoop<()>), Box<dyn Error>> {
+    log::debug!("Creating window and event loop");
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
         .with_title(title)
